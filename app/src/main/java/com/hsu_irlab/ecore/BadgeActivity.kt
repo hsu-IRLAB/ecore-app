c53eaffc0db0cad71d83d919f4196c1162c0ae98
coldDelight <hno05039@naver.com>
2022-08-04 22:33:20 +0900

    feat : bottom navigation

---
+++ app/src/main/java/com/hsu_irlab/ecore/BadgeActivity.kt
@@ -0,0 +1,11 @@
+package com.hsu_irlab.ecore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BadgeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_badge)
+    }
+}
