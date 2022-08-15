3cf6723983cf8b5b14f8855855d052199220e5c8
kguard <kkh9331@naver.com>
2022-08-15 17:14:12 +0900

    feat: 랭킹 페이지 추가

--- app/src/main/java/com/hsu_irlab/ecore/RankingFragment.kt
+++ app/src/main/java/com/hsu_irlab/ecore/RankingFragment.kt
@@ -4,47 +4,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.hsu_irlab.ecore.databinding.FragmentRankingBinding
-import com.hsu_irlab.ecore.presentaion.adapter.RankingAdapter
-import com.hsu_irlab.ecore.presentaion.viewmodel.RankingViewModel
+import com.hsu_irlab.ecore.ranking.FollowingRankingFragment
+import com.hsu_irlab.ecore.ranking.WholeRankingFragment
 
 class RankingFragment : Fragment() {
 
     private val binding by lazy { FragmentRankingBinding.inflate(layoutInflater) }
 
-    private val viewModel by lazy { ViewModelProvider(this,
-        RankingViewModel.Factory())[RankingViewModel::class.java] }
-
-    private lateinit var retrofitAdapter: RankingAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-        setView() // 리사이클러 뷰 연결
-        setObserver() //
     }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return binding.root
     }
-    private fun setView(){
-        retrofitAdapter =  RankingAdapter().apply {
-            setHasStableIds(true) // 리사이클러 뷰 업데이트 시 깜빡임 방지
-        }
-        binding.activityMainRanking.adapter = retrofitAdapter // 리사이클러 뷰 연결
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /*val pagerAdapter=RankingFragmentStateAdapter(requireActivity())
+        pagerAdapter.addFragment(WholeRankingFragment())
+        pagerAdapter.addFragment(FollowingRankingFragment())*/
+        binding.vp2Ranking.adapter=RankingPagerAdapter(requireActivity())
+        TabLayoutMediator(binding.tlRanking,binding.vp2Ranking){ tab,position ->
+            when(position)
+            {
+                0-> tab.text="전체"
+                1-> tab.text="팔로워"
+            }
+        }.attach()
     }
-    private fun setObserver() {
-        // 뷰모델 관찰
-        viewModel.retrofitRanking.observe(this) {
-            viewModel.retrofitRanking.value?.let { it1 -> retrofitAdapter.setData(it1) }
+    private inner class RankingPagerAdapter(fa:FragmentActivity): FragmentStateAdapter(fa){
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when(position)
+            {
+                0->WholeRankingFragment()
+                else->FollowingRankingFragment()
+            }
         }
 
     }
+
 
 }
---
+++ app/src/main/java/com/hsu_irlab/ecore/ranking/FollowingRankingFragment.kt
@@ -0,0 +1,50 @@
+package com.hsu_irlab.ecore.ranking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.hsu_irlab.ecore.databinding.FragmentFollowingRankingBinding
+import com.hsu_irlab.ecore.databinding.FragmentWholeRankingBinding
+import com.hsu_irlab.ecore.presentaion.adapter.RankingAdapter
+import com.hsu_irlab.ecore.presentaion.viewmodel.RankingViewModel
+
+class FollowingRankingFragment:Fragment() {
+
+    private val binding by lazy { FragmentFollowingRankingBinding.inflate(layoutInflater) }
+
+    private val viewModel by lazy { ViewModelProvider(this,
+        RankingViewModel.Factory())[RankingViewModel::class.java] }
+
+    private lateinit var retrofitAdapter: RankingAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        setView() // 리사이클러 뷰 연결
+        setObserver() //
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+    private fun setView(){
+        retrofitAdapter =  RankingAdapter().apply {
+            setHasStableIds(true) // 리사이클러 뷰 업데이트 시 깜빡임 방지
+        }
+        binding.rvFollowingRanking.adapter = retrofitAdapter // 리사이클러 뷰 연결
+    }
+    private fun setObserver() {
+        // 뷰모델 관찰
+        viewModel.retrofitRanking.observe(this) {
+            viewModel.retrofitRanking.value?.let { it1 -> retrofitAdapter.setData(it1) }
+        }
+
+    }
+
+}
