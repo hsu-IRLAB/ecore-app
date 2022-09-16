147d96242d3876d0ecb12286978d3234c05c6765
coldDelight <hno05039@naver.com>
2022-09-17 00:09:01 +0900

    no message

--- domain/src/main/java/com/hsu_irlab/domain/use_case/CommonUseCase.kt
+++ domain/src/main/java/com/hsu_irlab/domain/use_case/CommonUseCase.kt
@@ -6,6 +6,18 @@ import com.hsu_irlab.domain.repository.CommonRepository
 class CommonUseCase(
     private val repository: CommonRepository
 ) {
+    suspend fun getDailyProfileImg(target:Int):List<DomainImages>
+    {
+        return repository.getProfileImg("daily","all",target)
+    }
+    suspend fun getChallengeProfileImg(target:Int):List<DomainImages>
+    {
+        return repository.getProfileImg("challenge","all",target)
+    }
+    suspend fun getCampaignProfileImg(target:Int):List<DomainImages>{
+        return repository.getProfileImg("campaign","all",target)
+    }
+
     suspend fun getProfileImg(type:String,target:Int):List<DomainImages>
     {
         return repository.getProfileImg(type,"all",target)
